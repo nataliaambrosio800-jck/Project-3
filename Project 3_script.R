177a9f57f294a4191ae0e6f3c7da403ce007b2aa
NatiGati <natalia.ambrosio800@gmail.com>
2025-11-13 13:30:58 -0600

    Add files via upload
    r script file

---
+++ Project 3_script.R
@@ -0,0 +1,10 @@
+
+#install packages and data
+
+rawdata <- read.csv("C:/Users/natal/Desktop/SDS313/Sound_Ordinance_Permits_20251106.csv")
+
+library(tidyverse)
+library(ggplot2)
+library(stringr)
+library(dplyr)
+
